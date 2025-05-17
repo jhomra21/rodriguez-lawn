35dec1522efe9d04ca2d5497094ed8d147afb32d
Juan <jhonra@icloud.com>
2025-05-17 11:05:00 -0500

    way better seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,17 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://rodriguez-lawn.pages.dev',
 	vite: {
 		plugins: [tailwindcss()],
 	  },
 	// Enable React to support React JSX components.
-	integrations: [react()]
+	integrations: [
+		react(),
+		sitemap()
+	]
 });
