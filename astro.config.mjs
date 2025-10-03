a50e21449b04e7f6813848ac1e8d46d43a1953d1
jhomra21 <jhonra@icloud.com>
2025-10-03 12:47:11 -0500

    fix sitemap config for en

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,26 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwindcss from '@tailwindcss/vite';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://rodriguezlawnservicergv.com',
-	vite: {
-		plugins: [tailwindcss()],
-	  },
-	// Enable React to support React JSX components.
-	integrations: [
-		react(),
-		sitemap({
-			i18n: {
-				defaultLocale: 'en',
-				locales: {
-					en: 'en',
-					es: 'es',
-				},
-			},
-		})
-	]
+  site: "https://rodriguezlawnservicergv.com",
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  // Enable React to support React JSX components.
+  integrations: [
+    react(),
+    sitemap({
+      i18n: {
+        defaultLocale: "en",
+        locales: {
+          en: "",
+          es: "es",
+        },
+      },
+    }),
+  ],
 });
